29042dd640a101de808e26ce0073df46c8031e01
MisterVladimir <vladimir@shteyn.net>
2020-01-14 12:55:58 -0500

    Install zoom client, comment out Scala, Haskel

--- apt-gets.sh
+++ apt-gets.sh
@@ -186,19 +186,26 @@ sudo apt-get -y install libjson-c-dev \
 	&& make -j$(nproc) \
 	&& sudo make install
 
+# Alternative (better?) bluetooth
 sudo add-apt-repository ppa:bluetooth/bluez \
 	&& sudo apt-get update \
 	&& sudo apt-get install bluez \
 	&& sudo cp default.pa /etc/pulse/default.pa
 
 # sbt (Scala)
-echo "deb https://dl.bintray.com/sbt/debian /" | \
-		sudo tee -a /etc/apt/sources.list.d/sbt.list \
-	&& curl -sL \
-		"https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | \
-		sudo apt-key add \
-	&& sudo apt-get update \
-	&& sudo apt-get -y install sbt
+# echo "deb https://dl.bintray.com/sbt/debian /" | \
+#                 sudo tee -a /etc/apt/sources.list.d/sbt.list \
+#         && curl -sL \
+#                 "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | \
+#                 sudo apt-key add \
+#         && sudo apt-get update \
+#         && sudo apt-get -y install sbt
 
 # Haskel
-sudo apt-get update && sudo apt-get -y install haskell-platform
+# sudo apt-get update && sudo apt-get -y install haskell-platform
+
+# Zoom client
+curl -LO https://zoom.us/client/latest/zoom_amd64.deb \
+	&& sudo apt-get update \
+	&& sudo apt -y install ./zoom_amd64.deb \
+
