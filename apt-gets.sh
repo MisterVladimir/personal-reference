01afa230fda674a7eb920e0a396db6dc16d71fe2
MisterVladimir <vladimir@shteyn.net>
2020-03-10 09:14:39 -0400

    Google Cloud SDK

--- apt-gets.sh
+++ apt-gets.sh
@@ -292,3 +292,12 @@ cd Downloads \
 	&& curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
 	&& sudo dpkg -i google-chrome-stable_current_amd64.deb
 
+### Google Cloud SDK
+# Add the Cloud SDK distribution URI as a package source
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get -y update && sudo apt-get -y install google-cloud-sdk
